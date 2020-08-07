8fdb9a6b3dba2410f12a3e3ffa08d079d3e44587
Matthew Vedder <matthewvedder05@gmail.com>
2020-08-07 15:28:17 +0200

    New Version Diff: Message of who won, Hidden input

--- tests/prs.py
+++ tests/prs.py
@@ -1,34 +1,63 @@
-# Best of three
-def check_valid_command(command):  # command = paper
+from getpass import getpass
+
+
+def check_valid_command(command):
     if command.lower() not in ["scissor", "paper", "rock"]:
         return False
     else:
         return True
 
 
-def check_winner_command(player1, player2):  # player1 = scissor, player2 = rock
+def check_winner_command(player1, player2):
     if player1.lower() == "scissor" and player2.lower() == "rock":
-        return player2
+        return "Player 2"
     elif player1.lower() == "scissor" and player2.lower() == "paper":
-        return player1
+        return "Player 1"
     elif player1.lower() == "rock" and player2.lower() == "paper":
-        return player2
+        return "Player 2"
+    elif player1.lower() == "rock" and player2.lower() == "scissor":
+        return "Player 1"
+    elif player1.lower() == "paper" and player2.lower() == "rock":
+        return "Player 1"
+    elif player1.lower() == "paper" and player2.lower() == "scissor":
+        return "Player 2"
+    else:
+        return None
+
 
 def main():
     running = True
     print("Welcome")
+    print("[Player 1] Please choose one of the following options: Rock, Paper or Scissor")
+    player1 = None
+    player2 = None
     while running:
-        print("[Player 1] Please choose one of the following options: Rock, Paper or Scissor")
-        command = input()
+        command = getpass("selection ")
         if command == 'quit':
             running = False
             print("Thanks for playing!")
         else:
-            pass
-    # Ist die eingabe korrekt
-    # Speichere eingabe für player1 und player 2
-    # wurde 2 mal was eingegeben?
-    # checke wer gewonnen hat?
+            if player1 is None and player2 is None:
+                if check_valid_command(command):
+                    player1 = command
+                    print("[Player 1] chose successfully")
+                    print("[Player 2] Please choose one of the following options: Rock, Paper or Scissor")
+                else:
+                    print ("Please retry and choose one of the following options: rock,paper or scissor")
+            elif player1 != None and player2 is None:
+                if check_valid_command(command):
+                    player2 = command
+                    print("[Player 2] chose successfully")
+                else:
+                    print ("Please retry and choose one of the following options: rock,paper or scissor")
+                winner = check_winner_command(player1, player2)
+                print("Player 1 selection: " + player1)
+                print("Player 2 selection: " + player2)
+                if winner is None:
+                    print ("Draw")
+                else:
+                    print("You won, " + winner + "!")
+                running = False
 
 
 if __name__ == "__main__":
--- tests/test_prs.py
+++ tests/test_prs.py
@@ -24,41 +24,58 @@ def test_should_check_false_command():
 def test_should_check_player_1_won():
     player1 = "scissor"
     player2 = "paper"
-    assert player1 == check_winner_command(player1, player2)
+    assert "Player 1" == check_winner_command(player1, player2)
 
 
 def test_should_check_player_2_won():
     player1 = "scissor"
     player2 = "rock"
-    assert player2 == check_winner_command(player1, player2)
+    assert "Player 2" == check_winner_command(player1, player2)
 
 
 def test_should_check_player_2_won_capitol_R():
     player1 = "scissor"
     player2 = "Rock"
-    assert player2 == check_winner_command(player1, player2)
+    assert "Player 2" == check_winner_command(player1, player2)
 
 
-def test_should_check_Draw_won_capitol_R():
+def test_should_check_player2_rock_paper():
+    player1 = "rock"
+    player2 = "paper"
+    assert "Player 2" == check_winner_command(player1, player2)
+
+
+def test_should_check_player1_rock_scissor():
+    player1 = "rock"
+    player2 = "scissor"
+    assert "Player 1" == check_winner_command(player1, player2)
+
+
+def test_should_check_player1_paper_rock():
+    player1 = "paper"
+    player2 = "rock"
+    assert "Player 1" == check_winner_command(player1, player2)
+
+
+def test_should_check_player1_paper_scissor():
+    player1 = "paper"
+    player2 = "scissor"
+    assert "Player 2" == check_winner_command(player1, player2)
+
+
+def test_should_check_draw_scissor():
     player1 = "scissor"
     player2 = "scissor"
     assert None == check_winner_command(player1, player2)
 
 
-def test_should_check_player2_rock_4win_capitol_R():
-    player1 = "rock"
-    player2 = "paper"
-    assert player2 == check_winner_command(player1, player2)
-
-
-def test_should_check_player2_rock_draw_capitol_R():
+def test_should_check_draw_rock():
     player1 = "rock"
     player2 = "rock"
     assert None == check_winner_command(player1, player2)
 
 
-def test_should_check_player2_paper_draw_capitol_R():
+def test_should_check_draw_paper():
     player1 = "paper"
     player2 = "paper"
     assert None == check_winner_command(player1, player2)
-
